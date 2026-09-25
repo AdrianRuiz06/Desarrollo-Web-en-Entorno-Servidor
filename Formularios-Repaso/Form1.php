b6050c4fc1a1bedad85baa72daaf9b9cebfddfa5
Adrian <adrianruiz0706@gmail.com>
2026-09-25 10:32:39 +0200

    Añadir scripts sync.sh y setup_xampp_link.sh

---
+++ Formularios-Repaso/Form1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulario 1</title>
+</head>
+<body>
+    <form action = "recogidaDatos1.php" method="get">
+        <label for="nombre"> Introduce Tu nombre</label>
+        <input type="text" id="nombre" name="nombre" require>
+        <input type="submit" name="enviar" value="Enviar el formulario1">
+
+    </form>
+</body>
+</html>
